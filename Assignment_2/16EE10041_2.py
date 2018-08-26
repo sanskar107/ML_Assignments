08b5d98fb73a856abf41617b3d9938919796626a
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2018-08-26 14:12:16 +0530

    Assignment_2 Completed

--- Assignment_2/16EE10041_2.py
+++ Assignment_2/16EE10041_2.py
@@ -1,5 +1,8 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# 16EE10041
+# Sanskar Agrawal
+# Assignment Number 2
+# Compilation : python <program_name>
+
 import numpy as np
 import math
 
@@ -7,11 +10,14 @@ class Node:
 	def __init__(self, data):
 		self.left = None
 		self.right = None
-		self.parent = None
 		self.data = data
 
 	def PrintTree(self):
-		print(self.data)
+		if(self.left):
+			self.left.PrintTree()
+		print self.data
+		if(self.right):
+			self.right.PrintTree()
 
 def entropy(m, n):
 		if(m == 0 and n == 0):
@@ -32,12 +38,23 @@ mark = np.zeros([1, n_features], dtype = np.int32)
 # print data
 # print X
 
-def function(X, Y, root):
-	temp = root
-	marked = []
-	while(temp != None):
-		marked.append(temp.data)
-		temp = temp.parent
+def function(X, Y, marked):
+	if(X.shape[1] == len(marked)):
+		# print "All_Marked"
+		return None
+	flag_0 = flag_1 = 1
+	for i in range(0, Y.shape[0]):
+		if(Y[i] == 1):
+			flag_0 = 0
+		else:
+			flag_1 = 0
+
+	if(flag_0):
+		# print "Pure negative"
+		return Node(-1)
+	if(flag_1):
+		# print "Pure Positive"
+		return Node(-2)
 	E_class = 0.0
 	p = n = 0.0
 	for i in range(0, X.shape[0]):
@@ -46,7 +63,7 @@ def function(X, Y, root):
 		else:
 			n += 1
 	E_class = entropy(p, n)
-	print("Class Entropy : ", E_class)
+	# print("Class Entropy : ", E_class)
 	max_gain = -1
 	max_gain_attr = -1
 	for i in range(0, X.shape[1]):
@@ -74,11 +91,50 @@ def function(X, Y, root):
 		if(max_gain < gain):
 			max_gain = gain
 			max_gain_attr = i
-		print gain
-		if(root == None):
-			root = Node(max_gain_attr)
+		# print gain
+	# print "Max gain = ", max_gain
+	# print "Max gain_attr = ", max_gain_attr
+	marked.append(max_gain_attr)
+	root = Node(max_gain_attr)
+	X_pos = []
+	X_neg = []
+	Y_pos = []
+	Y_neg = []
+	for i in range(0, X.shape[0]):
+		if(X[i][max_gain_attr] == 0):
+			X_neg.append(X[i])
+			Y_neg.append(Y[i])
 		else:
-			
+			X_pos.append(X[i])
+			Y_pos.append(Y[i])
+	X_neg = np.reshape(np.array(X_neg), (-1, X.shape[1]))
+	X_pos = np.reshape(np.array(X_pos), (-1, X.shape[1]))
+	Y_neg = np.array(Y_neg)
+	Y_pos = np.array(Y_pos)
+	root.left = function(X_neg, Y_neg, marked)
+	root.right = function(X_pos, Y_pos, marked)
 
+	return root
 
-function(X, Y, None)
+# print Y.shape
+# print X.shape
+root = function(X, Y, [])
+test = np.genfromtxt('test2.csv', delimiter = ',')
+ans = []
+for i in range(0, test.shape[0]):
+	temp = root
+	while(temp):
+		if(temp.data == -1):
+			ans.append(0)
+			break
+		if(temp.data == -2):
+			ans.append(1)
+			break
+		temp = temp.left if test[i][temp.data] == 0 else temp.right
+file = open('16EE10041_2.out', 'w')
+for i in ans:
+	file.write(str(i) + ' ')
+file.close()
+file = open('16EE10041_2.out', 'r')
+data = file.read()
+print data
